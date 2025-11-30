04d3ac80e7450a96f60f8069e1377aea3f894838
Caleb Dickson <157729906+CalebDksn25@users.noreply.github.com>
2025-11-30 12:53:08 -0800

    Initial data pull from yf set up

--- datapull.py
+++ datapull.py
@@ -2,9 +2,16 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
 
 #Define the end date (today's date) and the list of tickers we want to pull from
-tickers = ['QQQ', 'SPY', 'GLD', 'NQD', 'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA', 'META']
+tickers = ['QQQ', 'SPY', 'GLD', 'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA', 'META', 'NVDA']
 end_date = datetime.today()
-print(f"End Date: {end_date}, Tickets: {tickers}")
+
+#Set the start date
+start_date = end_date - timedelta(days=1*365)
+
+df = yf.download(tickers=tickers, start=start_date, end=end_date)
+df.to_csv("data/all_prices.csv")
+print(df)
